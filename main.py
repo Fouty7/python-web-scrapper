243668a4bd6356cd948c2e0a3a9dbbf79451505c
Aminu Ndakun <Amin4712@gmail.com>
2022-08-05 21:11:19 +0100

    using Beauitful Soup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as html_file:
-    content = html_file.read ()
-    print(content)
+    content = html_file.read()
+    
+    soup = BeautifulSoup(content, 'lxml')
+    print(soup.prettify())
