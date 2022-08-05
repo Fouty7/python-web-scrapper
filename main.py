f586d4f000b262d6cbceb70c1a379af107c9f8e5
Aminu Ndakun <Amin4712@gmail.com>
2022-08-05 21:31:27 +0100

    using Beauitful Soup

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ with open('home.html', 'r') as html_file:
     content = html_file.read()
     
     soup = BeautifulSoup(content, 'lxml')
-    print(soup.prettify())
+    #courses_tags = soup.find_all('h5')
+    #for course in courses_tags:
+    #    print(course.text)
+    course_cards = soup.find_all('div', class_='card')
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        print(f'{course_name} costs {course_price}')
