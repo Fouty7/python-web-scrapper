d7e4ba0600e3f2247589b3b1b40b3243c2798c7a
Aminu Ndakun <Amin4712@gmail.com>
2022-08-07 20:34:59 +0100

    request scrapper

--- request.py
+++ request.py
@@ -1,3 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+
+soup = BeautifulSoup(html_text, "lxml")
+#jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx').text
+for jobs in soup.find_all('li', class_='clearfix job-bx wht-shd-bx'):
+    # jobs_result = str(jobs.text.encode("utf-8"))
+    jobs_result = jobs.text
+    print(' '.join(re.findall('\w+ ', jobs_result)))
+    # print(jobs_result)
